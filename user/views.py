cf190303067ed34cfd753609743692b14030b48d
Dmytry <donchykd@gmail.com>
2023-04-22 19:14:54 +0300

    Creating filters.py with user's search filter and adding that functionality to views.py and urls, also intalling django-filter package and adding it to requirements.txt

--- user/views.py
+++ user/views.py
@@ -1,16 +1,25 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, permissions
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
+from user.filters import UserFilter
 from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = UserFilter
 
 
 class CreateTokenView(ObtainAuthToken):
@@ -25,13 +34,3 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
-
-
-class UserSearchView(generics.ListAPIView):
-    serializer_class = UserSerializer
-
-    def get_queryset(self):
-        query = self.request.query_params.get('q')
-        if query:
-            return User.objects.filter(username__icontains=query)
-        return User.objects.none()
