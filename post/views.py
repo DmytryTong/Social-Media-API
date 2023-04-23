3b2a95bcc1071334cdefb3d8efa082b2fbb5d576
Dmytry <donchykd@gmail.com>
2023-04-23 16:57:41 +0300

    Modifying views according new functional

--- post/views.py
+++ post/views.py
@@ -1,19 +1,59 @@
-from rest_framework import generics
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import generics, mixins, viewsets
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from .models import Post
-from .serializers import PostSerializer
+from rest_framework.viewsets import GenericViewSet
+
+from .models import Post, Tag
+from .serializers import PostSerializer, PostListSerializer, TagSerializer
 
 
-class PostList(generics.ListAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.all().prefetch_related("tags")
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated, )
+
+    @staticmethod
+    def _params_to_ints(params):
+        return [int(str_id) for str_id in params.split(",")]
 
     def get_queryset(self):
-        user = self.request.user
-        subscriptions = user.subscribers.all()
-        return Post.objects.filter(author__in=[user] + list(subscriptions))
+        subscribers_ids = (
+            self.request.user.profile.subscribers.all()
+        )
+        user_id = self.request.user.profile.id
+
+        queryset = self.queryset.filter(
+            author__in=list(subscribers_ids) + [user_id]
+        )
+        tags = self.request.query_params.get("tags")
+        if tags:
+            tags_ids = self._params_to_ints(tags)
+            queryset = (queryset.filter(tags__id__in=tags_ids))
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+        return PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "tags",
+                type={"type": "list", "items": {"type": "number"}},
+                description="Filter by tags id (ex. ?tags=1,2)"
+            )
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
 
-class CreatePost(generics.CreateAPIView):
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
--- user/views.py
+++ user/views.py
@@ -1,17 +1,27 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
-from django.db.models import Q
+from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, permissions, status
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema
+from rest_framework import generics, status, viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view, action
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
+from rest_framework.views import APIView
 
 from user.filters import UserFilter
-from user.serializers import UserSerializer, AuthTokenSerializer, UserListSerializer, UserDetailSerializer
+from user.models import UserProfile
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+    UserListSerializer,
+    LogoutSerializer, UserProfileDetailSerializer, UserProfileListSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -28,9 +38,9 @@ class UserList(generics.ListAPIView):
 
 
 class UserDetail(generics.RetrieveAPIView):
-    serializer_class = UserDetailSerializer
+    serializer_class = UserSerializer
     queryset = get_user_model().objects.all()
-    lookup_field = 'id'
+    lookup_field = "id"
 
 
 class CreateTokenView(ObtainAuthToken):
@@ -47,24 +57,70 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
         return self.request.user
 
 
-class SubscribeView(generics.GenericAPIView):
+class ManageUserProfileView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserSerializer
-    queryset = get_user_model().objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
-    def post(self, request, user_id):
-        user = get_object_or_404(User, id=user_id)
-        subscriber = request.user
+    def get_object(self):
+        return self.request.self
 
-        # Перевірте, чи не підписується користувач на себе
-        if subscriber == user:
-            return Response({"error": "Can't subscribe to yourself"}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Перевірте, чи вже підписався користувач на цього користувача
-        if subscriber.subscriptions.filter(id=user.id).exists():
-            return Response({"error": "Already subscribed"}, status=status.HTTP_400_BAD_REQUEST)
+class UserProfileViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
-        subscriber.subscriptions.add(user)
-        serializer = self.get_serializer(user)
+    def get_queryset(self):
+        queryset = self.queryset
+        username = self.request.query_params.get("username")
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        if username:
+            queryset = queryset.filter(username__icontains=username)
 
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return UserProfileDetailSerializer
+        return UserProfileListSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @action(methods=["GET"], detail=True, url_path="subscribing")
+    def subscribe_toggle(self, request, pk=None):
+        me = get_user_model().objects.get(pk=request.user.pk)
+        guest = get_user_model().objects.get(pk=pk)
+        if me != guest:
+            if me in guest.profile.subscribers.all():
+                guest.profile.subscribers.remove(me.id)
+            else:
+                guest.profile.subscribers.add(me.id)
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "username",
+                type=OpenApiTypes.STR,
+                description="Filter by username (ex. username=username1)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
+class LogoutView(APIView):
+    def delete(self, request):
+        serializer = LogoutSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(status=204)
